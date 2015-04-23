0c36db4d8739836768788887f49d9bac15839679
David Asabina <david@supr.nu>
2015-04-23 12:48:02 +0200

    Testing Redis storage

--- lib/adaptor.rb
+++ lib/adaptor.rb
@@ -5,12 +5,15 @@ class RedisAdaptor
   end
 
   def save_if_nonexistent(id, val)
-    redis.setnx(id, val)
+    (true if @redis.setnx(id, val) == "OK") or false
   end
 
   def save_and_possibly_overwrite(id, val)
+    (true if @redis.set(id, val) == "OK") or false
   end
 
   def get(id)
+    @redis.get(id)
+    #(true if @redis.get(id) == "OK") or false
   end
 end
--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -1,7 +1,33 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe 'Redis Adaptor' do
-  #let(:redis) { RedisAdaptor.new(nil, Redis.new) }
-  it "plays ball" do
+  let(:test_redis) { Redis.new }
+  let(:adaptor) { RedisAdaptor.new(nil, test_redis) }
+
+  before(:each) do
+    test_redis.flushdb
+  end
+  it "sets values" do
+    expect(adaptor.save_and_possibly_overwrite('overwritable', 'haha')).to eq(true)
+  end
+
+  it "breaks if item already set" do
+    expect(adaptor.save_and_possibly_overwrite('saving_something', 'hi')).to eq(true)
+    expect(adaptor.save_if_nonexistent('saving_something', 'oops')).to eq(false)
+  end
+
+  it "gets nonexistent values" do
+    expect(adaptor.get('nothing')).to eq(nil)
+  end
+
+  it "gets nonexistent values" do
+    expect(adaptor.save_and_possibly_overwrite('something', 'Westergasfabriek')).to eq(true)
+    expect(adaptor.get('something')).to eq('Westergasfabriek')
+  end
+
+  context "used under the Persistence layer" do
+    let(:storage) { Persistence.new(adaptor) }
+
+    it ""
   end
 end
