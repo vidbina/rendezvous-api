6cfe74534298503aa4a28ddd2556cb2763ae0087
David Asabina <david@supr.nu>
2015-04-23 14:42:22 +0200

    Saving and retrieving hashes :)

--- lib/adaptor.rb
+++ lib/adaptor.rb
@@ -5,14 +5,32 @@ class RedisAdaptor
   end
 
   def save_if_nonexistent(id, val)
-    (true if @redis.setnx(id, val) == "OK") or false
+    if val.kind_of? Hash
+      if !@redis.exists? id
+        (true if @redis.hmset(id, val.each.map{ |k, v| [k, v] }.flatten) == "OK") or false
+      else
+        false
+      end
+    else
+      (true if @redis.setnx(id, val) == "OK") or false
+    end
   end
 
   def save_and_possibly_overwrite(id, val)
-    (true if @redis.set(id, val) == "OK") or false
+    if val.kind_of? Hash
+      arr = *val.each.map{ |k, v| [k, v] }.flatten
+      res = @redis.hmset(id, arr)
+      (true if @redis.exists(id)) or false
+    else
+      (true if @redis.set(id, val) == "OK") or false
+    end
   end
 
-  def get(id)
-    @redis.get(id)
+  def get(id, type=nil)
+    if @redis.type(id) == "hash"
+      @redis.hgetall(id)
+    else
+      @redis.get(id)
+    end
   end
 end
--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -26,6 +26,11 @@ describe 'Redis Adaptor' do
     expect(adaptor.get('something')).to eq('Westergasfabriek')
   end
 
+  it "saves hashes" do
+    expect(adaptor.save_and_possibly_overwrite('a_hash', { who: "Bob", where: "fabriek" })).to eq(true)
+    expect(adaptor.get('a_hash')).to eq({ 'who' => 'Bob', 'where' => 'fabriek' })
+  end
+
   context "used under the Persistence layer" do
     let(:storage) { Persistence.new(adaptor) }
 
@@ -37,6 +42,11 @@ describe 'Redis Adaptor' do
     it "does not overwrite saved items" do
       expect(storage.save('untouchable', 'demon')).to be(true)
       expect(storage.save('untouchable', 'angel', overwrite=false)).to be(false)
+    end
+
+    it "properly handles hash saving and retrieval" do
+      expect(storage.save('hashable', { name: 'conrad' })).to be(true)
+      expect(storage.get('hashable')).to eq({ 'name' => 'conrad' })
     end
   end
 end
